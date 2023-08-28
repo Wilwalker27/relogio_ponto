a5129416198906e6f4755b40aa94549c9e3cd3de
Nubks <ek.silva.santos@gmail.com>
2023-08-28 00:13:39 -0300

    Corrigindo bug de retorno

--- App/Controller/PontoController.php
+++ App/Controller/PontoController.php
@@ -12,17 +12,21 @@ class PontoController // registrar a chegada dos funcionários no BD
             return $pontoService->entrada();
 
         } catch (\Throwable $th) {
-            return $th;
+            return "Erro na entrada do funcionario";
         }
     }
 
     public function saida(FuncionarioModel $funcionario){ // registrar a saída dos funcionários
-        $conn = new Connection();
+        try {
+            $conn = new Connection();
+            $pontoService = new PontoService($conn, $funcionario);
+            return $pontoService->saida();
 
-        $pontoService = new PontoService($conn, $funcionario);
-
-        return $pontoService->saida();
+        } catch (\Throwable $th) {
+            return "Erro na Saida do funcionario";
+        }
     }
+
     public function delete($id){ // registrar a saída dos funcionários
         $conn = new Connection();
 
