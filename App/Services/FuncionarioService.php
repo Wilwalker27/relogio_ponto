d817ee56f0b9dfb8f03bbdc9eb0389f45815d19e
William <82401512+Wilwalker27@users.noreply.github.com>
2023-08-28 17:13:18 -0300

    Atualizar o FuncionarioService.php

--- App/Services/FuncionarioService.php
+++ App/Services/FuncionarioService.php
@@ -48,7 +48,7 @@ class FuncionarioService
 
             return $funcionario;
         } catch (\Throwable $th) {
-            return $th;
+            return [];
         }
     }
     public function getFuncionario()
